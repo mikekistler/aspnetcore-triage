f1a0899be79416ff630753af4a1b3fdd92177ce3
Mike Kistler <mikekistler@microsoft.com>
2024-12-30 15:55:43 -0600

    Repro for issue 59570

--- issue-59570/Program.cs
+++ issue-59570/Program.cs
@@ -14,28 +14,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/hello", () => "Hello World!");
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/goodbye", async context =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    await context.Response.WriteAsJsonAsync(new { Message = "Goodbye!" });
+}).WithDescription("Goodbye");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
