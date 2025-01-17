bb34f75f1b0bb636b1bbfb2bec1736226a25ca9c
Mike Kistler <mikekistler@microsoft.com>
2025-01-17 12:45:35 -0600

    Issue 59770 repro (or not)

--- issue-59770/Program.cs
+++ issue-59770/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,28 +16,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapPost("/bug", ([FromBody]Stream body) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    using var reader = new StreamReader(body);
+    var json = reader.ReadToEndAsync().Result;
+    Console.WriteLine(json);
+    return TypedResults.Ok();
 })
-.WithName("GetWeatherForecast");
+.WithName("bug");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
