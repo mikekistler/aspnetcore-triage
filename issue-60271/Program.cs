eeceefe8d0c4e3efd4400096bd5314487771ad79
Mike Kistler <mikekistler@microsoft.com>
2025-02-14 11:34:18 -0600

    Attempted repro for issue 60271

--- issue-60271/Program.cs
+++ issue-60271/Program.cs
@@ -1,15 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddApiVersioning((option) =>
+{
+    option.DefaultApiVersion = new ApiVersion(1, 0);
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi(opt =>
+{
+    // opt.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
+    opt.OpenApiVersion =Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0;
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi().RequireAuthorization("bearer");
 }
 
 app.UseHttpsRedirection();
