124051f4bbae07126d9d768ae6ec3b97d5af3632
Mike Kistler <mikekistler@microsoft.com>
2025-02-14 12:53:55 -0600

    Repro for issue 60163

--- issue-60163/Program.cs
+++ issue-60163/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,28 +16,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/bug", () => { return "Hello World!"; }).Produces<Dictionary<AuthErrorType, ErrorDetail>>();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+[JsonConverter(typeof(JsonStringEnumConverter<AuthErrorType>))]
+public enum AuthErrorType
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    InvalidCredentials,
+    InvalidLogin
+}
+
+internal class ErrorDetail
+{
+    public string Message { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
 }
