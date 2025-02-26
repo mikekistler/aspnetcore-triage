903d4901d1760d2ab475656aea7fc5c2e1e958cf
Mike Kistler <mikekistler@microsoft.com>
2025-02-26 08:01:20 -0600

    Option 1

--- issue-60394/Program.cs
+++ issue-60394/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,28 +17,33 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var api = app.MapGroup("api/talks");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// The ProblemHttpResult is not present in the OpenAPI document, as explained in https://github.com/dotnet/aspnetcore/issues/58719
+// To ensure that it is added, we can use a combination of TypedResults and extension methods (which is not ideal).
+// We could also fall back to using Results and use extension methods for the OpenAPI metadata, but we'd lose compile-time safety..
+api.MapGet("/{id:int:min(1)}", GetTalk)
+    .WithName("Talks_GetTalk")
+    .ProducesProblem(StatusCodes.Status404NotFound);
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+static Results<Ok<TalkModel>, NotFound<ProblemDetails>> GetTalk(int id)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+     var SampleTalks = new
+    {
+        Talks = new[]
+        {
+            new TalkModel(1, "Talk 1", "Speaker 1", DateTime.UtcNow),
+            new TalkModel(2, "Talk 2", "Speaker 2", DateTime.UtcNow),
+            new TalkModel(3, "Talk 3", "Speaker 3", DateTime.UtcNow),
+        }
+    };
+    // This still excludes the traceid from the ProblemDetails instance for brevity purposes
+    var talk = SampleTalks.Talks.FirstOrDefault(x => x.Id == id);
+    return talk == null ?
+        TypedResults.NotFound<ProblemDetails>(new ProblemDetails{}) :
+        TypedResults.Ok(talk);
 }
+
+public record TalkModel(int Id, string Title, string Speaker, DateTime StartTime);
