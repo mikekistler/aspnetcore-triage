08adb4e870814b9985eec76b51f4500d0fbd6f34
Mike Kistler <mikekistler@microsoft.com>
2025-04-05 08:23:10 -0700

    Repro of issue 59976

--- issue-59976/Program.cs
+++ issue-59976/Program.cs
@@ -1,41 +1,31 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
+builder.Services.AddOpenApi( );
+var app = builder.Build( );
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
+if( app.Environment.IsDevelopment( ) ) {
+    app.MapOpenApi( );
+}
+app.UseHttpsRedirection( );
+
+app.MapPut("/test",
+           (MsgMovimentacaoLocaisTrabalho msg) => Results.Ok( ))
+   .WithName("GetWeatherForecast");
+
+app.Run( );
+
+
+public class MsgMovimentacaoLocaisTrabalho {
+    public IList<InfoGeral>? LocaisRemover { get; set; }
+    public InfoGeral? LocaisAssociar { get; set; }
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+public class InfoGeral {
+    public Guid GuidDirecao { get; set; }
+    public IEnumerable<int> Locais { get; set; } = Enumerable.Empty<int>();
 }
